25ea03f4bfc15295c09bc9e641a9d4e3a0fc9859
Agnes-Kalunda <agypeter97@gmail.com>
2023-02-01 20:15:29 +0300

    added sidebar

--- src/App.js
+++ src/App.js
@@ -1,41 +1,81 @@
-import Hero from "./components/Hero"
-import Header from "./components/Header"
-import Section from "./components/Section"
+// import Hero from "./components/Hero"
+// import Header from "./components/Header"
+// import Section from "./components/Section"
+// import Testimonial from "./components/Testimonial"
+// import ContactUs from "./components/ContactUs"
+// import Footer from "./components/Footer"
+// import AboutUs from "./components/AboutUs"
+// import '@fontsource/roboto/300.css';
+// import '@fontsource/roboto/400.css';
+// import '@fontsource/roboto/500.css';
+// import '@fontsource/roboto/700.css';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import { createTheme, ThemeProvider} from '@mui/material/styles';
+// import Sidebar from './components/Sidebar';
+
+// const theme = createTheme({
+//   typography: {
+//     fontFamily: [
+//       'Poppins',
+//       'sans-serif',
+//     ].join(','),
+//   },});
+
+// function App() {
+//   return (
+//     <>
+//     <ThemeProvider theme={theme}>
+//       <CssBaseline />
+//       {/* <Header /> */}
+//       <Hero />
+//       <Section />
+//       <AboutUs />
+//       <Testimonial />
+//       <ContactUs />
+//       <Footer />
+//       </ThemeProvider>
+//     </>
+
+//   );
+// }
+
+// export default App;
+
+
+
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Hero from "./components/AboutUs"
+import Header from "./components/Section"
+import Section from "./components/Testimonial"
 import Testimonial from "./components/Testimonial"
 import ContactUs from "./components/ContactUs"
 import Footer from "./components/Footer"
 import AboutUs from "./components/AboutUs"
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import CssBaseline from '@mui/material/CssBaseline';
-import { createTheme, ThemeProvider} from '@mui/material/styles';
 
-const theme = createTheme({
-  typography: {
-    fontFamily: [
-      'Poppins',
-      'sans-serif',
-    ].join(','),
-  },});
 
 function App() {
   return (
-    <>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      {/* <Header /> */}
-      <Hero />
-      <Section />
-      <AboutUs />
-      <Testimonial />
-      <ContactUs />
-      <Footer />
-      </ThemeProvider>
-    </>
-
+    <Router>
+      <SideBar>
+        <Routes>
+          <Route path="/" element={<AboutUs />} />
+          <Route path="/Section" element={<Section />} />
+          <Route path="/Hero" element={<Hero />} />
+          <Route path="/Testimonial" element={<Testimonial />} />
+          <Route path="/ContactUs" element={<ContactUs />} />
+          <Route path="/Footer" element={<Footer />} />
+          
+          
+        </Routes>
+        <div>
+        </Sidebar>
+        </div>
+     
+    </Router>
   );
 }
-
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from 'react';
 
 ReactDOM.render(
   <React.StrictMode>
